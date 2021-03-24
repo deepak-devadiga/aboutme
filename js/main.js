aea17b22f823ce11f154e008d8f25313755a7519
Deepak <deepucsait@gmail.com>
2021-03-24 21:36:56 +0530

    Changed styles

--- js/main.js
+++ js/main.js
@@ -1,62 +1,62 @@
 
 
-(function($) {
+(function ($) {
 	"use strict";
 	/*---------------------------------------------------- */
 	/* Preloader
-	------------------------------------------------------ */ 
-   $(window).load(function() {
-      // will first fade out the loading animation 
-    	$("#loader").fadeOut("slow", function(){
-        // will fade out the whole DIV that covers the website.
-        $("#preloader").delay(300).fadeOut("slow");
-      });       
-  	})
+	------------------------------------------------------ */
+	$(window).load(function () {
+		// will first fade out the loading animation 
+		$("#loader").fadeOut("slow", function () {
+			// will fade out the whole DIV that covers the website.
+			$("#preloader").delay(300).fadeOut("slow");
+		});
+	})
 	/*---------------------------------------------------- */
 	/* Owl Carousel
-	------------------------------------------------------ */ 
+	------------------------------------------------------ */
 	$("#owl-slider").owlCarousel({
-        navigation: false,
-        pagination: true,
-        itemsCustom : [
-	        [0, 1],
-	        [700, 2],
-	        [960, 3]
-	     ],
-        navigationText: false
-    });
+		navigation: false,
+		pagination: true,
+		itemsCustom: [
+			[0, 1],
+			[700, 2],
+			[960, 3]
+		],
+		navigationText: false
+	});
 
 	$("#owl-slider1").owlCarousel({
-        navigation: true,
-        pagination: true,
-        itemsCustom : [
-	        [0, 1],
-	        [500, 2],
-	        [700, 3]
-	     ],
-        navigationText: false,
-		autoPlay:true,
-		slideSpeed:100,
-		paginationSpeed:500
-    });
+		navigation: true,
+		pagination: true,
+		itemsCustom: [
+			[0, 1],
+			[500, 2],
+			[700, 3]
+		],
+		navigationText: false,
+		autoPlay: true,
+		slideSpeed: 100,
+		paginationSpeed: 500
+	});
 
 
 
 	/*---------------------------------------------------- */
-  	/* Smooth Scrolling
-  	------------------------------------------------------ */
-  	$('.smoothscroll').on('click', function (e) {
-	 	
-	 	e.preventDefault();
+	/* Smooth Scrolling
+	------------------------------------------------------ */
+	$('.smoothscroll').on('click', function (e) {
 
-   	var target = this.hash,
-    	$target = $(target);
+		e.preventDefault();
 
-    	$('html, body').stop().animate({
-       	'scrollTop': $target.offset().top
-      }, 800, 'swing', function () {
-      	window.location.hash = target;
-      });
+		var target = this.hash,
+			$target = $(target);
 
-  	});	
+		$('html, body').stop().animate({
+			'scrollTop': $target.offset().top
+		}, 800, 'swing', function () {
+			window.location.hash = target;
+		});
+
+	});
 })(jQuery);
